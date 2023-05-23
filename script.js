18df70d364922556929d4b0809f35c0cb6187fd5
Paz Valenzuela <pazvalenzu@gmail.com>
2023-05-23 16:56:37 -0700

    bubbleSort

---
+++ script.js
@@ -0,0 +1,164 @@
+const cardContainer = document.getElementById("cardContainer");
+const cardContainerR = document.getElementById("cardMatriz");
+const suits = ["♦", "♥", "♠", "♣"];
+const ranks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function generateRandomCard() {
+  const suitIndex = Math.floor(Math.random() * suits.length);
+  const rankIndex = Math.floor(Math.random() * ranks.length);
+  const suit = suits[suitIndex];
+  const rank = ranks[rankIndex];
+
+  return {
+    suit: suit,
+    rank: rank,
+  };
+}
+function getRankLabel(rank) {
+  switch (rank) {
+    case 1:
+      return "A";
+    case 11:
+      return "J";
+    case 12:
+      return "Q";
+    case 13:
+      return "K";
+    default:
+      return rank.toString();
+  }
+}
+
+function createCardHTML(card) {
+  const cardHTML = document.createElement("div");
+  console.log("suit " + card.suit);
+  cardHTML.className = "card suit-" + card.suit.toLowerCase();
+
+  const suitTop = document.createElement("div");
+  suitTop.className = "suit-top";
+  suitTop.textContent = card.suit;
+
+  const rankElement = document.createElement("div");
+  rankElement.className = "rank";
+  rankElement.textContent = getRankLabel(card.rank);
+
+  const suitBottom = document.createElement("div");
+  suitBottom.className = "suit-bottom";
+  suitBottom.textContent = card.suit;
+
+  cardHTML.appendChild(suitTop);
+  cardHTML.appendChild(rankElement);
+  cardHTML.appendChild(suitBottom);
+
+  
+
+  return cardHTML;
+}
+
+//BUBLESORT!!!!!!!!
+function bubbleSort(cards) {
+    console.log(cards);
+  
+    let n = cards.length;
+    let swapped;
+    do {
+      swapped = false;
+      for (let i = 0; i < n - 1; i++) {
+        if (cards[i].rank > cards[i + 1].rank) {
+          let temp = cards[i];
+          cards[i] = cards[i + 1];
+          cards[i + 1] = temp;
+          swapped = true;
+        }
+        drawCardes(cards);
+      }
+      n--;
+    } while (swapped);
+  
+    return cards;
+  }
+
+var cards = [];
+var changes = bubbleSort(cards);
+
+for (var i = 0; i < changes.length; i++) {
+  console.log("Iteración " + (i + 1) + ": " + changes[i].join(", "));
+}
+
+// Función para mostrar el registro completo de cambios en el sitio web
+function showChangeLog(changes) {
+  var changeLog = document.getElementById("cardMatriz");
+  changeLog.innerHTML = "";
+
+  for (var i = 0; i < changes.length; i++) {
+    var changeItem = document.createElement("ol");
+    changeItem.textContent = JSON.stringify(changes[i]);
+    changeLog.appendChild(changeItem);
+  }
+}
+
+// Función para ordenar las cartas y mostrar el registro de cambios
+function sortCards() {
+  var cardContainer = document.getElementById("cardContainer");
+
+
+  bubbleSort(cards);
+
+}
+
+function drawCards() {
+  cards = [];
+  const numCardsInput = document.getElementById("numCards");
+  const numCards = parseInt(numCardsInput.value);
+
+  cardContainer.innerHTML = "";
+  cardContainerR.innerHTML = "";
+  for (let i = 0; i < numCards; i++) {
+    const card = generateRandomCard();
+    cards.push(card);
+    const cardHTML = createCardHTML(card);
+    cardContainer.appendChild(cardHTML);
+  }
+
+    console.log(cards);
+}
+
+function drawCardes(changes) {
+
+
+  if (changes && changes.length > 0) {
+    const row = document.createElement("div");
+    row.className = "card-row";
+    for (let i = 0; i < changes.length; i++) {
+      const card = changes[i];
+
+      const cardHTML = createCardHTMLes(card);
+      row.appendChild(cardHTML);
+    }
+    cardContainerR.appendChild(row);
+  }
+}
+
+function createCardHTMLes(card) {
+
+    const cardElement = document.createElement("div");
+  cardElement.className = "card";
+
+  const suitTop = document.createElement("div");
+  suitTop.className = "suit-top";
+  suitTop.textContent = card.suit;
+
+  const rankElement = document.createElement("div");
+  rankElement.className = "rank";
+  rankElement.textContent = getRankLabel(card.rank);
+
+  const suitBottom = document.createElement("div");
+  suitBottom.className = "suit-bottom";
+  suitBottom.textContent = card.suit;
+
+  cardElement.appendChild(suitTop);
+  cardElement.appendChild(rankElement);
+  cardElement.appendChild(suitBottom);
+
+  return cardElement;
+}
